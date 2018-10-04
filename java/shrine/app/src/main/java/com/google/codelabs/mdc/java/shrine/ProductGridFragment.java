232b011365002df989405918581df611726d2f78
Alvin Dizon <alvin.dizon@ghl.com>
2018-10-04 11:20:17 +0800

    Added Interpolator for smoother reveal animation
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 import com.google.codelabs.mdc.java.shrine.network.ProductEntry;
 import com.google.codelabs.mdc.java.shrine.staggeredgridlayout.StaggeredProductCardRecyclerViewAdapter;
@@ -73,7 +74,8 @@ public class ProductGridFragment extends Fragment {
 
         toolbar.setNavigationOnClickListener(new NavigationIconClickListener(
                 getContext(),
-                view.findViewById(R.id.product_grid)));
+                view.findViewById(R.id.product_grid),
+                new AccelerateDecelerateInterpolator()));
     }
 
     @Override
